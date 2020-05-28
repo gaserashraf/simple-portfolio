952dd088ba7d2c35787985b78380d034df990723
gaserashraf <elkinggaser@gmail.com>
2020-05-28 23:15:27 +0200

    modfiy the ancors

--- js/js.js
+++ js/js.js
@@ -1,3 +1,8 @@
+function posTop() 
+{
+return typeof window.pageYOffset != 'undefined' ?  window.pageYOffset : document.documentElement && document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop ? document.body.scrollTop : 0;
+} 
+//start icons scroll
 var pro=document.getElementById("p"),
     PROGECTS=document.getElementById("pro"),
     aboutBut=document.getElementById('a'),
@@ -58,3 +63,38 @@ acBut.onclick=function()
     behavior: 'smooth'
     });
 }
+//end icons scroll
+console.log(posTop());
+window.addEventListener("scroll", function() {
+    if(posTop() >=1146&&posTop() <=1800)
+        pro.classList.add('active');
+    else
+        pro.classList.remove('active');
+    })
+
+window.addEventListener("scroll", function() {
+    if(posTop() >=100&&posTop() <=350)
+        aboutBut.classList.add('active');
+    else
+        aboutBut.classList.remove('active');
+    })
+window.addEventListener("scroll", function() {
+    if(posTop() >=600&&posTop() <=888)
+        educaBut.classList.add('active');
+    else
+        educaBut.classList.remove('active');
+    })
+
+window.addEventListener("scroll", function() {
+    if(posTop() >=2089&&posTop() <=2400)
+        skillsBut.classList.add('active');
+    else
+        skillsBut.classList.remove('active');
+    })
+
+window.addEventListener("scroll", function() {
+    if(posTop() >=2401&&posTop() <=2716)
+        acBut.classList.add('active');
+    else
+        acBut.classList.remove('active');
+    })
